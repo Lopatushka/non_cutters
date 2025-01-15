cfe5a62f1d6171da583eefb31b4700588a3055c0
Lopatushka <helena.lopatukhina@gmail.com>
2025-01-15 10:58:33 +0100

    change message

--- non_cutters.py
+++ non_cutters.py
@@ -9,7 +9,7 @@ def process_file(file_path):
             lines = {line[:-1] for line in lines}  # Remove trailing commas and convert to a set
         return lines
     except FileNotFoundError:
-        print(f"Error: File {file_path} not found.")
+        print(f"Error: File {file_path} not found and will not be processed.")
         return set()
 
 def main(file_paths):
@@ -31,35 +31,10 @@ def main(file_paths):
 
     # Print the result
     if result:
-        print(f"List of non-cutters for all files: {', '.join(sorted(result))}")
+        print(f"List of non-cutter restriction enzymes: {', '.join(sorted(result))}")
     else:
         print("No common non-cutters found across all files.")
 
 if __name__ == "__main__":
-    file_list = ['pMal_non_cutters.txt', 'pCMV-3Tag-1A_non_cutters.txt', 'pAcUW51_non_cutters.txt']
+    file_list = ['./files/pMal_non_cutters.txt', './files/pCMV-3Tag-1A_non_cutters.txt', './files/pAcUW51_non_cutters.txt']
     main(file_list)
-
-
-'''def main():
-    content_list = [] # list of sets
-    for name in path_to_files:
-        with open(name, 'r') as file:
-            # Read lines and strip newline characters
-            temp = file.read().split()
-            temp = temp[3:]
-            temp = [i[:-1] for i in temp] # delete commas
-            temp = set(temp)
-            content_list.append(temp)
-
-    # for i in content_list:
-    #     print(i)
-
-    # Perform AND (intersection) on all sets in the listcle
-    result = list(set.intersection(*content_list))
-
-    # Print the result
-    print(f'List of non-cutters for all files: {", ".join(result)}')
-
-if __name__ == "__main__":
-    path_to_files = ['pMal_non_cutters.txt', 'pCMV-3Tag-1A_non_cutters.txt', 'pAcUW51_non_cutters.txt']
-    main()'''
